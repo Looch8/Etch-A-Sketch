9c44847cfad2aaa09f2adebad7d9b624a52fad4a
Luke <lukewheldale@gmail.com>
2022-12-27 20:42:17 +1030

    create 16x16 grid with js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+// DOM elements
+const board = document.querySelector(`.board`);
+board.style.gridTemplateColumns = `repeat(16, 1fr)`;
+board.style.gridTemplateRows = `repeat(16, 1fr)`;
+
+// create 16x16 grid
+for (let i = 0; i < 256; i++) {
+  let square = document.createElement(`div`);
+  square.style.background = `blue`;
+  board.insertAdjacentElement(`beforeend`, square);
+}
+
+// hover color change function
